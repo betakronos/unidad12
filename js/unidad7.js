b0688e169a1ade872fd01afd2786065ae6a5ddb3
betakronos <118102023+betakronos@users.noreply.github.com>
2022-11-13 07:03:46 -0300

    Add files via upload

---
+++ js/unidad7.js
@@ -0,0 +1,18 @@
+var nombreUsuario = prompt('¿Cual es tu nombre?')
+
+alert('Bienvenido/a '+ nombreUsuario)
+
+var twdseen = prompt('¿Alguna vez esuchaste hablar de THE WALKING DEAD?')
+
+alert('Bien, continuemos '+ nombreUsuario)
+
+var temporadasTotales= 11
+var twdSeasons= prompt('¿Cuantas temporadas viste de TWD?')
+
+var temporadasRestantes = temporadasTotales-twdSeasons
+
+var twdTrama = prompt('¿Sabes cual es la trama de esta serie?')
+alert('Te lo contamos más adelante de todos modos... ')
+
+var twdTempfinal = prompt('¿Sabías que quedan solo 8 capitulos para que la serie termine?')
+
---
+++ js/unidad8.js
@@ -0,0 +1,11 @@
+var cajaTxt = document.getElementById('caja_cont6')
+
+function mostrarTxt() {
+cajaTxt.style.display = 'block'
+}
+
+var cajaVid = document.getElementById('caja_cont7')
+
+function mostrarVid() {
+cajaVid.style.display = 'block'
+}
